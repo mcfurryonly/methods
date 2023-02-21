8a78d9c4eb6a832704e157745186033599bdc055
mcfurry <mcfurry.only@gmail.com>
2023-02-21 13:43:31 +0300

    homework task 1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,17 @@ public class Main {
 
 
     public static void main(String[] args) {
+        printLeapOrNotLeapYear(20449);
 
     }
-    public boolean isLeapYear (int year){
-
+    public static boolean isLeapYear (int year){
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+    public static void printLeapOrNotLeapYear(int year){
+        if (isLeapYear(year)){
+            System.out.println(year + " год високостный");
+            return;
+        }
+        System.out.println(year + " год невисокостный");
     }
 }
